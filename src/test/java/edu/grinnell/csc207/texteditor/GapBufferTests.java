4889f7a31d73bf6a833184c1929598b03990332b
yutong <yutong@localhost>
2025-02-22 14:30:57 -0600

    SimpleStringBuffer Tests and GapBuffer Tests Complete.

--- src/test/java/edu/grinnell/csc207/texteditor/GapBufferTests.java
+++ src/test/java/edu/grinnell/csc207/texteditor/GapBufferTests.java
@@ -1,5 +1,127 @@
 package edu.grinnell.csc207.texteditor;
 
+import net.jqwik.api.ForAll;
+import net.jqwik.api.Property;
+import net.jqwik.api.constraints.IntRange;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Test;
+
 public class GapBufferTests {
-    /** TODO: fill me in with unit and property tests! */
+
+    /**
+     * TODO: fill me in with unit and property tests!
+     */
+
+    @Test
+    public void InsertSize() {
+        GapBuffer buffer = new GapBuffer();
+        buffer.insert('a');
+        buffer.insert('b');
+        assertEquals(2, buffer.getSize());
+    }
+
+    @Test
+    public void InsertString() {
+        GapBuffer buffer = new GapBuffer();
+        buffer.insert('a');
+        buffer.insert('b');
+        assertEquals("ab", buffer.toString());
+    }
+
+    @Test
+    public void InsertMoreThanGap() {
+        GapBuffer buffer = new GapBuffer();
+        buffer.insert('a');
+        buffer.insert('b');
+        buffer.insert('c');
+        buffer.insert('d');
+        buffer.insert('e');
+        buffer.insert('f');
+        assertEquals("abcdef", buffer.toString());
+    }
+
+    @Test
+    public void MoveLeft() {
+        GapBuffer buffer = new GapBuffer();
+        buffer.insert('a');
+        buffer.insert('b');
+        buffer.moveLeft();
+        assertEquals(1, buffer.getCursorPosition());
+    }
+
+    @Test
+    public void DeleteNothing() {
+        GapBuffer buffer = new GapBuffer();
+        buffer.insert('a');
+        buffer.insert('b');
+        buffer.moveLeft();
+        buffer.moveLeft();
+        buffer.delete();
+        assertEquals(0, buffer.getCursorPosition());
+        assertEquals("ab", buffer.toString());
+    }
+
+    @Test
+    public void DeleteAll() {
+        GapBuffer buffer = new GapBuffer();
+        buffer.insert('a');
+        buffer.insert('b');
+        buffer.delete();
+        buffer.delete();
+        assertEquals(0, buffer.getCursorPosition());
+    }
+
+    @Test
+    public void DeleteOnEmptyBuffer() {
+        GapBuffer buffer = new GapBuffer();
+        buffer.delete();
+        assertEquals("", buffer.toString());
+        assertEquals(0, buffer.getCursorPosition());
+    }
+
+    @Test
+    public void MoveRight() {
+        GapBuffer buffer = new GapBuffer();
+        buffer.insert('a');
+        buffer.insert('b');
+        buffer.moveLeft();
+        buffer.moveRight();
+        assertEquals(2, buffer.getCursorPosition());
+    }
+
+    @Test
+    public void EmptyBuffer() {
+        GapBuffer buffer = new GapBuffer();
+        assertEquals("", buffer.toString());
+        assertEquals(0, buffer.getCursorPosition());
+    }
+
+    @Test
+    public void GetCharAtValidIndex() {
+        GapBuffer buffer = new GapBuffer();
+        buffer.insert('x');
+        buffer.insert('y');
+        buffer.insert('z');
+        assertEquals('y', buffer.getChar(1));
+    }
+
+    @Test
+    public void GetCharAtInvalidIndex() {
+        GapBuffer buffer = new GapBuffer();
+        buffer.insert('x');
+        assertThrows(IndexOutOfBoundsException.class, () -> {buffer.getChar(5);});
+    }
+    
+    @Property
+    public boolean CursorPosition(@ForAll @IntRange(min = 2, max = 1000) int sz) {
+        GapBuffer buffer = new GapBuffer();
+        for (int i = 0; i < sz; i++) {
+            buffer.insert('a');
+        }
+        buffer.moveLeft();
+        buffer.moveLeft();
+        return buffer.getCursorPosition() == sz - 2;
+    }
+
 }
